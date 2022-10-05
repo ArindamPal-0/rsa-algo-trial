e2d2a16c8a8ef95f928fc85895cf0193064e0495
Arindam Pal <arindampal2000@gmail.com>
2022-10-05 20:39:58 +0530

    revert back commeted tests

--- main.py
+++ main.py
@@ -25,34 +25,34 @@ def main() -> int:
     # print(image_present)
 
     if image_present:
-        # results: list = download_images.get_image_results_with_coordinates(url, API_KEY, coordinates)
+        results: list = download_images.get_image_results_with_coordinates(url, API_KEY, coordinates)
 
-        # selected_result: dict = download_images.select_image(results, 0)
+        selected_result: dict = download_images.select_image(results, 0)
 
         # downloading 4th band
         band4_name: str = 'B04.jp2'
-        # band4_name: str = download_images.download_band_image(selected_result, 3, API_KEY)
+        band4_name: str = download_images.download_band_image(selected_result, 3, API_KEY)
 
         # downloading 8th band
         band8_name: str = 'B08.jp2'
-        # band8_name: str = download_images.download_band_image(selected_result, 7, API_KEY)
+        band8_name: str = download_images.download_band_image(selected_result, 7, API_KEY)
 
         print("BOTH THE IMAGES DOWNLOADED")
 
         # NOTE: Converting jp2 to tif format for clipping
         band4_converted: str = 'B04.tif'
-        # band4_converted: str = clip_images.convert_jp2_2_tif(band4_name)
+        band4_converted: str = clip_images.convert_jp2_2_tif(band4_name)
 
         band8_converted: str = 'B08.tif'
-        # band8_converted: str = clip_images.convert_jp2_2_tif(band8_name)
+        band8_converted: str = clip_images.convert_jp2_2_tif(band8_name)
 
         print("BOTH IMAGES CONVERTED FROM JP2 TO TIF")
 
         band4_clipped: str = 'clipped_B04.tif'
-        # band4_clipped: str = clip_images.clip_image(band4_converted, coordinates)
+        band4_clipped: str = clip_images.clip_image(band4_converted, coordinates)
 
         band8_clipped: str = 'clipped_B08.tif'
-        # band8_clipped: str = clip_images.clip_image(band8_converted, coordinates)
+        band8_clipped: str = clip_images.clip_image(band8_converted, coordinates)
 
         print("BOTH IMAGE CLIPPED")
 
